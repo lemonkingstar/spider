e094834d3ac16509800f3999eaa4a84a63ccf689
jiejun955 <jiejun955@foxmail.com>
2025-11-10 11:54:05 +0800

    chore: pgin

--- pkg/pgin/middleware/cors.go
+++ pkg/pgin/middleware/cors.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -11,18 +10,28 @@ import (
 	"github.com/lemonkingstar/spider/pkg/plog"
 )
 
-func CORSMiddleware() gin.HandlerFunc {
+func CORSMiddleware(allowedOrigins []string) gin.HandlerFunc {
+	allowedOriginMap := map[string]bool{}
+	for _, o := range allowedOrigins {
+		allowedOriginMap[o] = true
+	}
 	return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
 		if origin != "" {
 			// If "api-cors-header" is not given, but "api-enable-cors" is true, we set cors to "*"
 			// otherwise, all head values will be passed to HTTP handler
-			corsHeaders := "*"
-
+			corsHeaders := ""
+			if len(allowedOriginMap) == 0 {
+				corsHeaders = "*"
+			} else if allowedOriginMap[origin] {
+				corsHeaders = origin
+			}
 			// cors handle
 			plog.Debugf("CORS header is enabled and set to: %s, Origin: %s", corsHeaders, origin)
-			c.Writer.Header().Set("Access-Control-Allow-Origin", corsHeaders)
-			c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization")
+			if corsHeaders != "" {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", corsHeaders)
+			}
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 			c.Writer.Header().Set("Access-Control-Allow-Methods", "HEAD, GET, POST, DELETE, PUT, OPTIONS")
 
 			if c.Request.Method == http.MethodOptions {
@@ -34,14 +43,12 @@ func CORSMiddleware() gin.HandlerFunc {
 	}
 }
 
-func GinCors(domain string) gin.HandlerFunc {
-	var domains = []string{"*"}
-	if domain != "" {
-		domains = strings.Split(domain, ",")
+func GinCors(allowedOrigins []string) gin.HandlerFunc {
+	if len(allowedOrigins) == 0 {
+		allowedOrigins = []string{"*"}
 	}
-
 	return cors.New(cors.Config{
-		AllowOrigins:     domains,
+		AllowOrigins:     allowedOrigins,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
 		AllowHeaders:     []string{"*"},
 		AllowCredentials: true,
