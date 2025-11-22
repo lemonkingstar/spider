d49421a0a0b142c5d004e4f4d9df5f214580f24b
jiejun955 <jiejun955@foxmail.com>
2025-11-23 00:13:47 +0800

    mod: ptime

--- pkg/ptime/util.go
+++ pkg/ptime/util.go
@@ -1,1 +1,16 @@
 package ptime
+
+import "time"
+
+const (
+	DateLayout        = "2006-01-02"
+	TimeLayout        = "2006-01-02 15:04:05"
+	RFC3339Layout     = "2006-01-02T15:04:05"
+	RFC3339NanoLayout = "2006-01-02T15:04:05.000"
+)
+
+// ParseTime parses a formatted string to a time, use the system's local time zone.
+func ParseTime(s string) (time.Time, error) { return time.ParseInLocation(TimeLayout, s, time.Local) }
+
+// ParseDate parses a formatted string to a time, use the system's local time zone.
+func ParseDate(s string) (time.Time, error) { return time.ParseInLocation(DateLayout, s, time.Local) }
