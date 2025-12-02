e0ddc56a504ac2c607f7108532af8d81a08332f3
jiejun955 <jiejun955@foxmail.com>
2025-12-02 14:58:20 +0800

    chore: update

--- pkg/pdebug/pprof.go
+++ pkg/pdebug/pprof.go
@@ -20,11 +20,11 @@ type PProfServer struct {
 func (s *PProfServer) Handler() http.Handler {
 	mux := http.NewServeMux()
 	// std pprof
-	mux.HandleFunc("debug/pprof/", pprof.Index)
-	mux.HandleFunc("debug/pprof/cmdline", pprof.Cmdline)
-	mux.HandleFunc("debug/pprof/profile", pprof.Profile)
-	mux.HandleFunc("debug/pprof/symbol", pprof.Symbol)
-	mux.HandleFunc("debug/pprof/trace", pprof.Trace)
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	// custom debug
 	mux.HandleFunc("/debug/system", system)
 	mux.HandleFunc("/debug/version", version)
